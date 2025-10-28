e564f2ca04bb1e2516366b532745f20bccc92395
Mokarama <mokaramaaktershanta@gmail.com>
2025-10-28 10:27:23 +0600

    added class_constructor params

---
+++ class_constructor.js
@@ -0,0 +1,53 @@
+
+
+class person{
+
+    constructor(){
+        console.log("I am Constructor method")
+    }
+}
+
+let personObj=new person();
+
+
+
+//constructor paramiter diye kivabe property change kora jay
+/*** 
+ * 
+ 
+class person1{
+    num1=20;
+    num2=10;
+
+    constructor(a,b){
+        this.num1=a;
+        this.num2=b;
+    }
+
+    addTwoNum(){
+        return this.num1+this.num2;
+    }
+}
+
+let personObj1=new person1(50, 50);
+console.log(personObj1.addTwoNum());
+
+* 
+ */
+
+class person2{
+
+    num3=10;
+    num4=40;
+
+    constructor(a, b){
+        this.num3=a;
+        this.num4=b;
+    }
+    addToNumber(){
+        return this.num3*this.num4;
+    }
+}
+
+let person2Obj=new person2(5, 5);
+console.log(person2Obj.addToNumber())
---
+++ cons_para.js
@@ -0,0 +1,14 @@
+//Constructor params 
+
+class person{
+
+    constructor(num1, num2){
+        let sum= num1 + num2;
+        console.log(sum)
+    }
+
+   
+  
+}
+
+let personObj=new person(10,20)
