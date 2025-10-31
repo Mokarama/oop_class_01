87f36733f10732c71cc94edf10798eec52f023a8
Mokarama <mokaramaaktershanta@gmail.com>
2025-10-31 22:57:22 +0600

    added inh_con.js

--- index.js
+++ index.js
@@ -24,3 +24,15 @@ let person2={
 }
 console.log(person2);
 console.log(person2.getName())
+
+let person3={
+    first_name:"Shanta",
+    last_name:"Mim",
+    age:34,
+    isMuslim:true,
+    getName:()=>{
+        return `My full name is ${person2.first_name} ${person2.last_name}`
+    }
+}
+console.log(person3);
+console.log(person3.getName())
